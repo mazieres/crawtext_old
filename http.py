4a5e37e8104085fef2d88bc2108bf605a4221d1e
Antoine Mazieres <antoine.mazieres@gmail.com>
2012-10-02 08:29:47 +0200

    add crawler settings page

--- http.py
+++ http.py
@@ -5,13 +5,22 @@ from crawtext import *
 
 app = Flask(__name__)
 
+from random import choice
+pitchz = ['Yet another minimalist web crawler.','Your personnal web crawler.','Web crawler. For fun and science.']
+
 @app.route("/", methods=['GET'])
 def hello():
-	return render_template('index.html')
+	return render_template('index.html', pitch=choice(pitchz))
+
+@app.route("/set_crawler", methods=['GET'])
+def set_crawler():
+	return render_template('set_crawler.html')
 
 @app.route("/start_crawler", methods=['POST'])
 def start_crawler():
 	return render_template('start_crawler.html', query=request.form['query'], depth=request.form['depth'], email=request.form['email'])
+
+
 
 if __name__ == "__main__":
     app.run(host="127.0.0.1", debug=True)
